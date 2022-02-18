8faefb9c95613908dd59c863b84ae4ea0ae7b7a9
Попович Олег Игоревич <popovich.oi@esphere.ru>
2022-02-19 01:57:25 +0300

    feature/product-page - правки компонента Sidebar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { SidebarMenu } from "@src/components/SidebarMenu";
+import { HeadsetMic } from "@components/UI/icons";
+
+import "./sidebar";
+
+export const Sidebar = () => (
+  <div className="sidebar">
+    <div className="sidebar__header">
+      <div className="logo">MyAdminShop</div>
+    </div>
+    <div className="sidebar__body">
+      <SidebarMenu />
+    </div>
+    <div className="sidebar__footer">
+      <div className="menu__item">
+        <Link to="/chat">
+          <HeadsetMic />
+          <span>Чат</span>
+        </Link>
+      </div>
+    </div>
+  </div>
+);
