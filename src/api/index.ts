58896b811d7fcf8f54444787b425a719d1cfbf58
Попович Олег Игоревич <popovich.oi@esphere.ru>
2022-02-20 23:15:39 +0300

    feature/mock-service-worker - Внесены изменения в модуль отправки запросов

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,6 @@
-export { productsAPI, ProductsAPIGet } from "./products";
+export {
+  productsAPI,
+  PRODUCTS_URL,
+  PRODUCTS_SETTING_FIELDS_URL,
+} from "./products";
+export { BASE_URL, SERVER_URL } from "./api";
