356ab6fb83161db748b318b24957c10aa48223ad
Попович Олег Игоревич <popovich.oi@esphere.ru>
2022-03-30 22:48:04 +0300

    feature/chat - создание нового компонента Typing

---
+++ src/components/UI/Typing/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Typing";
+
+export const Typing = () => (
+  <div className="typing__indicator">
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+);
